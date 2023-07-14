1c6e416b496b2cb6d1350e0dfe55773c55675885
Just_a_man <117221950+justamanwho@users.noreply.github.com>
2023-07-14 21:53:57 +0200

    added third line

--- stash_example.py
+++ stash_example.py
@@ -1,2 +1,3 @@
 print('New line of code')
 print("Another line of code, you must remember that this is another line of code so don't think that it's the same as it was, if you think that way just read second line another time. Good luck! P.s.: Don't get lost in the lines, first line is the first and second is the second! I hope it helped me to not confuse them, because they are pretty similar, arent they?")
+print('third line')
